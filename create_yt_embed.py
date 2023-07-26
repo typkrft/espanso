1c2768963b80aa942a9c1eaad441710ee4ba444a
typkrft <12100960+typkrft@users.noreply.github.com>
2023-07-25 23:49:59 -0400

    created yt_embed script

---
+++ create_yt_embed.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def get_clipbaord() -> str:
+    p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+    retcode = p.wait()
+    data = p.stdout.read()
+    return data.decode("utf-8") 
+
+yt_link: str = get_clipbaord().split("=")[1]
+embed = f"""<iframe width="560" height="315" src="https://www.youtube.com/embed/{yt_link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"""
+print(embed)
