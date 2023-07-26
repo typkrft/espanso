699c9069e5a452376749f217e4c231755fe735fb
typkrft <12100960+typkrft@users.noreply.github.com>
2023-07-26 00:25:33 -0400

    split &

--- create_yt_embed.py
+++ create_yt_embed.py
@@ -6,6 +6,6 @@ def get_clipbaord() -> str:
     data = p.stdout.read()
     return data.decode("utf-8") 
 
-yt_link: str = get_clipbaord().split("=")[1]
+yt_link: str = get_clipbaord().split("=")[1].split("&")[0]
 embed = f"""<iframe width="560" height="315" src="https://www.youtube.com/embed/{yt_link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"""
 print(embed)
